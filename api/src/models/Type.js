4dd0481a667ad305363cfef7991fac59407fd074
Rei Orozco <rfoc15@gmail.com>
2022-01-18 14:00:15 -0500

    Database Settings

---
+++ api/src/models/Type.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "type",
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
+};
