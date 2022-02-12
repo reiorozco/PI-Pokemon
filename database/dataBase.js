5e3be66467a1dc0750f6ba9acbfb34603e36545e
Rei Orozco <rfoc15@gmail.com>
2022-02-12 18:16:45 -0500

    Responsive Version

--- database/dataBase.js
+++ database/dataBase.js
@@ -1,31 +1,41 @@
 const { Sequelize } = require("sequelize");
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = require("../config/dotenv");
+const {
+  NODE_ENV,
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_NAME,
+} = require("../config/dotenv");
+
+const optionsDev = {
+  logging: false, // Disables logging
+};
+
+const optionsProd = {
+  logging: false, // Disables logging
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+};
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  {
-    logging: false, // Disables logging
-    dialect: "postgres",
-    protocol: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  }
+  NODE_ENV === "development" ? optionsDev : optionsProd
 );
 
-async function testConnection() {
-  try {
-    await sequelize.authenticate();
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
-testConnection().then(() =>
-  console.log("Connection has been established successfully.")
-);
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("Unable to connect to the database:", err);
+  });
 
 module.exports = sequelize;
