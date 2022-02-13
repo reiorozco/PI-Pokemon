0fc5cd1a8b0f5cafa88cb0476def0bb297bdaf06
Rei Orozco <rfoc15@gmail.com>
2022-02-12 20:24:45 -0500

    Build fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const server = require("./app");
 const sequelize = require("./database/dataBase");
 
-const { PORT, HOST } = require("./config/dotenv");
+// const { PORT, HOST } = require("./config/dotenv");
 
 sequelize.sync({ force: true }).then(() => {
-  server.listen(PORT, HOST, () =>
-    console.log(`App listening on http://${HOST}:${PORT}`)
+  server.listen(process.env.PORT, process.env.HOST, () =>
+    console.log(`App listening on http://${process.env.HOST}:${process.env.PORT}`)
   );
 });
